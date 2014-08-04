e2d29306dff1b9a49005e949d5a94eb83603d58c
Elethom Hunter <elethomhunter@gmail.com>
2014-08-04 17:41:47 +0800

    Initial Commit

---
+++ PRSlideViewExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PRSlideViewExample
+//
+//  Created by Elethom Hunter on 8/4/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
---
+++ PRSlideViewExampleTests/PRSlideViewExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  PRSlideViewExampleTests.m
+//  PRSlideViewExampleTests
+//
+//  Created by Elethom Hunter on 8/4/14.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PRSlideViewExampleTests : XCTestCase
+
+@end
+
+@implementation PRSlideViewExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
